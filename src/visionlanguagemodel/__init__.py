d51ffa137890064e84e9e4a2e6be129dba206c92
rvishakhs <rvishakhs@gmail.com>
2025-08-13 12:13:42 +0100

    Kv cache

--- src/visionlanguagemodel/__init__.py
+++ src/visionlanguagemodel/__init__.py
@@ -4,3 +4,7 @@ import torch
 print(torch.cuda.is_available())  # True if CUDA is available
 print(torch.cuda.device_count())  # Number of CUDA devices
 print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No CUDA device")
+
+
+def __init__(self):
+    pass
--- src/visionlanguagemodel/model/modeling_gemma.py
+++ src/visionlanguagemodel/model/modeling_gemma.py
@@ -90,6 +90,64 @@ class paliGemmaForConditionalGeneration(nn.Module):
     def tie_weights(self): 
         return self.language_model.tie_weights()
     
+    def _merge_inputs_ids_with_image_features(
+            self, image_features: torch.Tensor, inputs_embeds: torch.Tensor, input_ids: torch.Tensor, attention_mask: torch.Tensor, kv_cache: Optional[KVCache] = None 
+    ): 
+        _, _, embed_dim = image_features.shape
+        batch_size, sequence_length = input_ids.shape
+        dtype, device = inputs_embeds.dtype, inputs_embeds.device
+
+        scaled_image_features = image_features / (self.config.hidden_size ** 0.5)
+
+        # combine the embedding of the image tokens, the text tokens and mask out all the padding tokens
+        final_embedding = torch.zeros(batch_size, sequence_length, embed_dim, dtype=inputs_embeds.dtype, device=inputs_embeds.device)
+        # shape : [batch_size, Seq_len] True for text tokens 
+        text_mask = (input_ids != self.config.image_token_index) & (input_ids != self.pad_token_id)
+
+        # shape: [Batch_size, Seq_len] True for image tokens
+        image_mask = input_ids == self.config.image_token_index
+
+        # shape : [Batch_size, Seq_len] True for padding tokens 
+        pad_mask = input_ids == self.pad_token_id
+
+        # we need to expand the masks 
+        text_mask_expanded = text_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+        image_mask_expanded = image_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+        pad_mask_expanded = pad_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+
+        # Add the text embedding 
+        final_embedding = torch.where(text_mask_expanded, inputs_embeds, final_embedding)
+        # Add the image embedding
+        final_embedding = final_embedding.masked_scatter(image_mask_expanded, scaled_image_features)
+        # zero out padding tokens
+        final_embedding = torch.where(pad_mask_expanded, torch.zeros_like(final_embedding), final_embedding)
+
+        #### Create the attention mask 
+        dtype, device = inputs_embeds.dtype, inputs_embeds.device
+        min_dtype = torch.finfo(dtype).min
+        q_len = inputs_embeds.shape[1]
+
+        if kv_cache is None or kv_cache.num_items() == 0:
+            casual_mask = torch.full(
+                (batch_size, q_len, q_len), fill_value=0, dtype=dtype, device=device
+
+            )
+        else: 
+            assert q_len == 1 
+            kv_len = kv_cache.num_items() + q_len
+
+            casual_mask = torch.full(
+                (batch_size, q_len, kv_len), fill_value=0, dtype=dtype, device=device
+            )
+
+        casual_mask = casual_mask.unsqueeze(1)
+
+        if kv_cache is not None and kv_cache.num_items() > 0:
+            position_ids = attention_mask.cumsum(-1)[:, -1]
+            if position_ids.dim() == 1:
+                position_ids = position_ids.unsqueeze(0)
+        else:
+            position_ids = (attention_mask.cumsum(-1)).masked_fill((attention_mask == 0), 1).to(device)
 
 
     def forward(
